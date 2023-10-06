4dfc750023d53722ff5ef942cbc2a073a83405fa
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-06 17:44:34 +0000

    button

--- script.js
+++ script.js
@@ -6,22 +6,40 @@ let listeTache = document.getElementById("liste_taches");
 
 btnAjout.addEventListener("click", function () {
   let ligne = listeTache.insertRow(-1);
-  let tacheAjoute = ligne.insertCell(0)
-  let action = ligne.insertCell(1)
-    let supprimer = document.getElementById('supp')
+  let tacheAjoute = ligne.insertCell(0);
+  let action = ligne.insertCell(1);
 
-  tacheAjoute.innerHTML = `<tr><td>${tache.value} </td></tr>`
-  action.innerHTML = `<button id='edit' onclick = 'modifier()'>Modifier</button><button id='supp'>Supprimer</button>`
-  tache.value=''
+  tacheAjoute.textContent = tache.value;
+  let edit = document.createElement("button");
+  action.append(edit);
+  edit.innerText = "Modifier";
+  edit.style.backgroundColor = "green";
+  edit.style.color = "white";
 
-  supprimer.addEventListener('click', function(){
-        // tacheAjoute.innerHTML = '' 
-        action.innerHTML = ''  
-  })
+  let supp = document.createElement("button");
+  action.append(supp);
+  supp.innerText = "Supprimer";
+  supp.style.backgroundColor = "red";
+  supp.style.color = "white";
+  tache.value = "";
+
+  supp.addEventListener("click", function () {
+    listeTache.removeChild(ligne);
+  });
+
+  edit.addEventListener("click", function () {
+    tache.value = tacheAjoute.textContent;
+    btnModifier.style.display = "block";
+    btnAjout.style.display = "none";
+  });
+
+
+  btnModifier.addEventListener("click", function () {
+
+    tacheAjoute.textContent = tache.value
+    btnModifier.style.display = "none";
+    btnAjout.style.display = "block";
+  });
+  
 });
-
-function modifier() {
-    btnModifier.style.display = "block"
-    btnAjout.style.display = "none"
-}
 
