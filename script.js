1ff166f6f0aeea4b84ca35ca33f77523b6ef23f0
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-10-12 10:42:29 +0000

    modifier

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-let tache = document.getElementById("tache");
-let btnAjout = document.getElementById("button-ajouter");
-let btnModifier = document.getElementById("button-modifier");
+const tacheInput = document.getElementById("tacheInput");
+const listContainer = document.getElementById("list-container");
 
-let listeTache = document.getElementById("liste_taches");
+function addTask() {
+  if (tacheInput.value === '') {
+    alert("Tu dois ecrire une tache")
+  }
+  else{
+    let li = document.createElement('li')
+    li.innerHTML = tacheInput.value
+    listContainer.appendChild(li)
+    let span = document.createElement('span')
+    span.innerHTML = "<button>Supprimer</button>"
+    li.appendChild(span)
+  }
+  tacheInput.value = ""
+}
 
-btnAjout.addEventListener("click", function () {
-  let ligne = listeTache.insertRow(-1);
-  let tacheAjoute = ligne.insertCell(0);
-  let action = ligne.insertCell(1);
-
-  tacheAjoute.textContent = tache.value;
-  let edit = document.createElement("button");
-  action.append(edit);
-  edit.innerText = "Modifier";
-  edit.style.backgroundColor = "green";
-  edit.style.color = "white";
-
-  let supp = document.createElement("button");
-  action.append(supp);
-  supp.innerText = "Supprimer";
-  supp.style.backgroundColor = "red";
-  supp.style.color = "white";
-  tache.value = "";
-
-  supp.addEventListener("click", function () {
-    listeTache.removeChild(ligne);
-  });
-
-  edit.addEventListener("click", function () {
-    tache.value = tacheAjoute.textContent;
-    btnModifier.style.display = "block";
-    btnAjout.style.display = "none";
-  });
+listContainer.addEventListener('click', function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked")
+  }
+  else if (e.target.tagName === "BUTTON"){
+    e.target.parentElement.parentElement.remove()
+  }
+}, false)
 
 
-  btnModifier.addEventListener("click", function () {
-
-    tacheAjoute.textContent = tache.value
-    btnModifier.style.display = "none";
-    btnAjout.style.display = "block";
-  });
-  
-});
 
